0237900dec741062cb4c0d19b1fea17e76617aec
laurikorpelainen <laurikorpelainen@gmail.com>
2025-12-11 19:44:52 +0200

    setup: tsconfig and index.ts

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
