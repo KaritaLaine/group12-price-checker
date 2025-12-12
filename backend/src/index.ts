8d87c25898a0d4407ef2c98644f5a5c8bcc6327c
KaritaLaine <44746@edu.raseko.fi>
2025-12-12 16:00:07 +0200

    build: connect to the database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,31 @@
-import express, { Request, Response} from "express";
-import dotenv from "dotenv";
+import dotenv from "dotenv"
+import type { Request, Response } from "express"
+import express from "express"
+import mongoose from "mongoose"
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
+
+if (!process.env.MONGODB_URI) {
+  console.error("MONGODB_URI is not defined in the .env file")
+  process.exit(1)
+}
+
+mongoose
+  .connect(process.env.MONGODB_URI, {})
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message)
+    process.exit(1)
+  })
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("API is running");
-});
+  res.send("API is running")
+})
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+  console.log(`Server running on http://localhost:${PORT}`)
+})
