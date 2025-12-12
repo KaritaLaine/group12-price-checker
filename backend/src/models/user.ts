d5a148df791c744d2d4696461ea92bcf1d1cd0c6
KaritaLaine <44746@edu.raseko.fi>
2025-12-12 16:00:14 +0200

    feat: add user schema

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,1 +1,19 @@
-// example model
+import bcrypt from "bcrypt"
+import type { Document } from "mongoose"
+import mongoose from "mongoose"
+
+interface User extends Document {
+  username: string
+  password: string
+}
+
+const userSchema = new mongoose.Schema<User>({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+})
+
+userSchema.pre<User>("save", async function (this: User) {
+  if (!this.isModified("password")) return
+
+  this.password = await bcrypt.hash(this.password, 10)
+})
