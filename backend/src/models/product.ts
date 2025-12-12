66a53537ee97182accb9a7fb592b678775b9ca3b
Eemeli <juho.e.nissinen@edu.turkuamk.fi>
2025-12-12 22:00:41 +0200

    style: run prettier

--- backend/src/models/product.ts
+++ backend/src/models/product.ts
@@ -1,26 +1,27 @@
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, { Document, Schema } from "mongoose"
 
 interface IProduct extends Document {
-    name: string
-    barcode: string
-    createdAt: Date
-    updatedAt: Date
+  name: string
+  barcode: string
+  createdAt: Date
+  updatedAt: Date
 }
 
-const productSchema = new Schema<IProduct>({
+const productSchema = new Schema<IProduct>(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     // GTIN number stored as a string
     barcode: {
-        type: String,
-        required: true,
-        unique: true,
-        match: /^[0-9]{8,14}$/
-    }
-},
-    { timestamps: true }
-) 
+      type: String,
+      required: true,
+      unique: true,
+      match: /^[0-9]{8,14}$/,
+    },
+  },
+  { timestamps: true }
+)
 
 export default mongoose.model<IProduct>("Product", productSchema)
--- backend/src/models/storePrice.ts
+++ backend/src/models/storePrice.ts
@@ -1,33 +1,34 @@
-import mongoose, { Document, ObjectId, Schema } from "mongoose";
+import mongoose, { Document, ObjectId, Schema } from "mongoose"
 
 interface IStorePrice extends Document {
-    productId: mongoose.Types.ObjectId
-    storeId: mongoose.Types.ObjectId
-    price: number
-    discount?: number
-    createdAt: Date
-    updatedAt: Date
+  productId: mongoose.Types.ObjectId
+  storeId: mongoose.Types.ObjectId
+  price: number
+  discount?: number
+  createdAt: Date
+  updatedAt: Date
 }
 
-const storePriceSchema = new Schema<IStorePrice>({
+const storePriceSchema = new Schema<IStorePrice>(
+  {
     productId: {
-        type: Schema.Types.ObjectId,
-        ref: "Product",
-        required: true,
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
     },
     storeId: {
-        type: Schema.Types.ObjectId,
-        ref: "Store",
-        required: true,
+      type: Schema.Types.ObjectId,
+      ref: "Store",
+      required: true,
     },
     price: {
-        type: Number
+      type: Number,
     },
     discount: {
-        type: Number
-    }
-},
-    { timestamps: true }
-) 
+      type: Number,
+    },
+  },
+  { timestamps: true }
+)
 
 export default mongoose.model<IStorePrice>("StorePrice", storePriceSchema)
