3a443b135eb715104c25d1c664f07ec7e1d4c2d9
Eemeli <juho.e.nissinen@edu.turkuamk.fi>
2025-12-12 22:01:30 +0200

    refactor: move user interface under types folder

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,19 +1,11 @@
 import bcrypt from "bcrypt"
-import type { Document } from "mongoose"
+import type { User } from "../types/user"
 import mongoose from "mongoose"
-
-interface User extends Document {
-  username: string
-  password: string
-  role: "admin" | "store_user"
-  status: "pending" | "unlocked" | "locked"
-  email: string
-}
 
 const userSchema = new mongoose.Schema<User>({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["admin", "store_user"], default: "store_user" },
+  role: { type: String, enum: ["admin", "storeUser"], default: "storeUser" },
   status: {
     type: String,
     enum: ["pending", "unlocked", "locked"],
@@ -22,11 +14,12 @@ const userSchema = new mongoose.Schema<User>({
 })
 
 userSchema.pre<User>("save", async function (this: User) {
-  // unlock new admin users by default and set store users to pending
+  // Set default status based on role (admin: unlocked, storeUser: pending)
   if (this.isNew && !this.status) {
     this.status = this.role === "admin" ? "unlocked" : "pending"
   }
 
+  // Hash password if it has been edited
   if (!this.isModified("password")) return
 
   this.password = await bcrypt.hash(this.password, 10)
