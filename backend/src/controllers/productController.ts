84b9bd65c39c709a937b35ca6f2c1a2384d08016
lukauu <156195421+lukauu@users.noreply.github.com>
2025-12-13 11:21:36 +0200

    feat: getProductPriceByName to retrieve product prices by query

--- backend/src/controllers/productController.ts
+++ backend/src/controllers/productController.ts
@@ -36,7 +36,6 @@ const getProductPriceByBarcode = async (req: Request, res: Response) => {
             return res.status(404).json({ message: "Product not found" })
         }
 
-        
         const prices = await StorePrice.find({ product: product._id })
 
         return res.status(200).json({ product, prices })
@@ -46,7 +45,28 @@ const getProductPriceByBarcode = async (req: Request, res: Response) => {
     }
 }
 
+const getProductPriceByName = async (req: Request, res: Response) => {
+    try {
+        const { name } = req.params
+        if (!name) {
+            return res.status(400).json({ message: "Name required" })
+        }
+
+        const product = await Product.findOne({ name })
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" })
+        }
+
+        const prices = await StorePrice.find({ product: product._id })
+        return res.status(200).json({ product, prices })
+    } catch (error: any) {
+        console.error(error)
+        return res.status(500).json({ message: "Server error" })
+    }
+}
+
 export const productController = {
     addProduct,
-    getProductPriceByBarcode
+    getProductPriceByBarcode,
+    getProductPriceByName
 }
