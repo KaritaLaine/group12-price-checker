e060e2981f93b5b94610067a017ee098add4edfd
laurikorpelainen <laurikorpelainen@gmail.com>
2025-12-13 17:09:18 +0200

    feat: add currency, iscurrent, pricesource and duplicated current price prevention

--- backend/src/models/storePrice.ts
+++ backend/src/models/storePrice.ts
@@ -1,10 +1,14 @@
 import mongoose, { Document, ObjectId, Schema } from "mongoose";
 
+type PriceSource = "store_batch" | "shopper";
 interface IStorePrice extends Document {
     productId: mongoose.Types.ObjectId
     storeId: mongoose.Types.ObjectId
     price: number
+    currency: string
+    isCurrent: boolean
     discount?: number
+    source?: PriceSource;
     createdAt: Date
     updatedAt: Date
 }
@@ -21,13 +25,32 @@ const storePriceSchema = new Schema<IStorePrice>({
         required: true,
     },
     price: {
-        type: Number
+        type: Number,
+        required: true
+    },
+    currency: {
+        type: String,
+        default: "EUR",
+        uppercase: true,
+        minlength: 3,
+        maxlength: 3
+    },
+    isCurrent: {
+        type: Boolean,
+        default: true
     },
     discount: {
         type: Number
-    }
+    },
+    source: {
+        type: String,
+        enum: ["store_batch", "shopper"]
+    },
 },
     { timestamps: true }
 ) 
+
+// Prevent multiple “current” prices for the same product/store
+storePriceSchema.index({ storeId: 1, productId: 1, isCurrent: 1 }, { unique: true, partialFilterExpression: { isCurrent: true } });
 
 export default mongoose.model<IStorePrice>("StorePrice", storePriceSchema)
