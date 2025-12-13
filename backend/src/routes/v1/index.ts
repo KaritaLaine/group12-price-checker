86655699800cb41d5942c71a57c12f0d57cd9755
Eemeli <juho.e.nissinen@edu.turkuamk.fi>
2025-12-13 18:47:56 +0200

    feat: add product routes

--- backend/src/routes/v1/index.ts
+++ backend/src/routes/v1/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express"
 import authRoutes from "./auth/auth"
+import productRoutes from "./product/product.routes"
 
 const router = Router()
 
 router.use("/auth", authRoutes)
+router.use("/products", productRoutes)
 
 export default router
