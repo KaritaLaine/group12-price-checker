428d9c5bafaae409dea28c17dc9376b67216ce3d
laurikorpelainen <laurikorpelainen@gmail.com>
2025-12-14 22:45:36 +0200

    feature: fix vitest config

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -4,10 +4,12 @@ export default defineConfig({
   test: {
     globals: true,
     include: ['test/**/*.test.ts'],
+    environment: 'node',
     coverage: {
       provider: 'v8',
       reporter: ['text', 'lcov'],
       reportsDirectory: 'coverage',
     },
+    typecheck: { tsconfig: 'tsconfig.test.json' }
   },
 });
