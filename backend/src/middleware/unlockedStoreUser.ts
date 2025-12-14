b5d00976fee4b7592abc9b6b31b70b6de89455e1
KaritaLaine <44746@edu.raseko.fi>
2025-12-14 20:42:54 +0200

    refactor: change middleware name from approvedStoreUser to unlockedStoreUser

---
+++ backend/src/middleware/unlockedStoreUser.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express"
+import User from "../models/user"
+import { handleResponse } from "../utils/response"
+
+export const requireUnlockedStoreUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const user = await User.findById(req.user!.userId)
+  if (!user) {
+    return handleResponse(res, 404, "User was not found")
+  }
+
+  if (user.status !== "unlocked") {
+    return handleResponse(
+      res,
+      403,
+      "Your account is locked. Please try again later or contact admin"
+    )
+  }
+
+  next()
+}
