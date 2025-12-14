4dbba2ae5002093271ee73fc47dc4621135b7a9c
KaritaLaine <44746@edu.raseko.fi>
2025-12-14 17:19:08 +0200

    feat: update store location and name

--- backend/src/controllers/storeController.ts
+++ backend/src/controllers/storeController.ts
@@ -1,15 +1,24 @@
-import Store from "../models/store"
 import { Request, Response } from "express"
+import mongoose from "mongoose"
+import Store from "../models/store"
 import { handleResponse } from "../utils/response"
 
 const updateStoreInfo = async (req: Request, res: Response) => {
-  const { name, location } = req.body
+  const { storeName, latitude, longitude } = req.body
+  const userId = req.user!.userId
+  const objectId = new mongoose.Types.ObjectId(userId)
 
-  const store = await Store.findOne({ owner: req.user!.userId })
+  const store = await Store.findOne({ owner: objectId })
   if (!store) return handleResponse(res, 404, "Store was not found")
 
-  if (name) store.name = name
-  if (location) store.location = location
+  if (storeName) store.name = storeName
+
+  if (latitude !== undefined && longitude !== undefined) {
+    store.location = {
+      type: "Point",
+      coordinates: [longitude, latitude],
+    }
+  }
 
   await store.save()
 
