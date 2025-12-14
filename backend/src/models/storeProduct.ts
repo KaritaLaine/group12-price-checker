246c5671ad6c702a4c44f9997892807ea55698e5
Eemeli <juho.e.nissinen@edu.turkuamk.fi>
2025-12-14 14:44:51 +0200

    refactor: rename storeprice as storeproduct

---
+++ backend/src/models/storeProduct.ts
@@ -0,0 +1,58 @@
+import mongoose, { Schema } from "mongoose"
+import { StoreProduct } from "../types/storeProduct"
+
+const storeProductSchema = new Schema<StoreProduct>(
+  {
+    product: {
+      type: mongoose.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    store: {
+      type: mongoose.Types.ObjectId,
+      ref: "Store",
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    currency: {
+      type: String,
+      default: "EUR",
+      uppercase: true,
+      minlength: 3,
+      maxlength: 3,
+    },
+    isCurrent: {
+      type: Boolean,
+      default: true,
+    },
+    discountedPrice: {
+      type: Number,
+    },
+    source: {
+      type: String,
+      enum: ["store_batch", "shopper"],
+    },
+    location: {
+      latitudes: {
+        type: Number,
+        required: true,
+      },
+      longitudes: {
+        type: Number,
+        required: true,
+      },
+    },
+  },
+  { timestamps: true }
+)
+
+// Prevent multiple current prices for the same product/store
+storeProductSchema.index(
+  { store: 1, product: 1, isCurrent: 1 },
+  { unique: true, partialFilterExpression: { isCurrent: true } }
+)
+
+export default mongoose.model<StoreProduct>("StoreProduct", storeProductSchema)
