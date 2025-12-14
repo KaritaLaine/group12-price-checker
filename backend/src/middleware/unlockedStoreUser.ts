875e96ce64e9f2e4b1d13e53b53c18b6ab65dd13
KaritaLaine <44746@edu.raseko.fi>
2025-12-14 20:44:31 +0200

    chore: add comment

--- backend/src/middleware/unlockedStoreUser.ts
+++ backend/src/middleware/unlockedStoreUser.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express"
 import User from "../models/user"
 import { handleResponse } from "../utils/response"
 
+// Only allow unlocked store users to access store routes
 export const requireUnlockedStoreUser = async (
   req: Request,
   res: Response,
