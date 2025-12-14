5a67cf591ab53e70624b5f73170733a2b5f1aa5b
KaritaLaine <44746@edu.raseko.fi>
2025-12-14 10:36:02 +0200

    feat: add admin routes

--- backend/src/routes/v1/admin/admin.ts
+++ backend/src/routes/v1/admin/admin.ts
@@ -1,1 +1,13 @@
-// example admin routes file (for example. get users, delete user, etc.)
+import { Router } from "express"
+import { adminController } from "../../../controllers/adminController.js"
+
+const router = Router()
+
+router.get("/users", adminController.getAllUsers)
+router.patch("/approve/:userId", adminController.approveStoreUser)
+router.patch("/decline/:userId", adminController.declineStoreUser)
+router.patch("/user-status/:userId", adminController.updateStoreUserStatus)
+router.delete("/delete/:userId", adminController.deleteStoreUser)
+router.post("/create-admin", adminController.createAdmin)
+
+export default router
--- backend/src/routes/v1/index.ts
+++ backend/src/routes/v1/index.ts
@@ -1,4 +1,7 @@
 import { Router } from "express"
+import { authenticate } from "../../middleware/auth"
+import { AuthorizationMiddleware } from "../../middleware/authorize"
+import adminRoutes from "./admin/admin"
 import authRoutes from "./auth/auth"
 import productRoutes from "./product/product.routes"
 
@@ -6,5 +9,11 @@ const router = Router()
 
 router.use("/auth", authRoutes)
 router.use("/products", productRoutes)
+router.use(
+  "/admin",
+  authenticate,
+  AuthorizationMiddleware.adminOnly,
+  adminRoutes
+)
 
 export default router
