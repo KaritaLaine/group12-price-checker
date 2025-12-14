8ee93fe7615e5c0ce6b0cd4a4a66959cf7ded3fe
Eemeli <juho.e.nissinen@edu.turkuamk.fi>
2025-12-14 14:45:16 +0200

    feat: remove addproduct route

--- backend/src/routes/v1/product/product.routes.ts
+++ backend/src/routes/v1/product/product.routes.ts
@@ -1,10 +1,12 @@
-import { Router } from "express";
-import { productController } from "../../../controllers/productController";
+import { Router } from "express"
+import { productController } from "../../../controllers/productController"
 
 const router = Router()
 
-router.post("/scan", productController.addProduct)
-router.get("/barcode/:barcode/compare", productController.getProductPriceByBarcode)
+router.get(
+  "/barcode/:barcode/compare",
+  productController.getProductPriceByBarcode
+)
 router.get("/name/:name/compare", productController.getProductPriceByName)
 
 export default router
