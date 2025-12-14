032d1d6a228ee117eba6f0bf95f9efca2b031117
Eemeli <juho.e.nissinen@edu.turkuamk.fi>
2025-12-14 14:46:00 +0200

    refactor: move storeproduct types into types folder

---
+++ backend/src/types/storeProduct.ts
@@ -0,0 +1,19 @@
+import mongoose, { Document } from "mongoose"
+
+type PriceSource = "store_batch" | "shopper"
+
+export interface StoreProduct extends Document {
+  product: mongoose.Types.ObjectId
+  store: mongoose.Types.ObjectId
+  price: number
+  currency: string
+  isCurrent: boolean
+  discountedPrice?: number
+  source?: PriceSource
+  location: {
+    latitudes: number
+    longitudes: number
+  }
+  createdAt: Date
+  updatedAt: Date
+}
