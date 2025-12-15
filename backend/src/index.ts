11001d3eec489fe29508e36fa07be30ac6f936df
Eemeli <juho.e.nissinen@edu.turkuamk.fi>
2025-12-15 16:41:16 +0200

    build: setup swagger

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,16 @@
 import "dotenv/config"
 import express from "express"
+import swaggerUi from "swagger-ui-express"
 import { connectToDatabase } from "./config/database"
+import { swaggerSpec } from "./config/swagger"
 import apiRoutes from "./routes/index"
 
 const PORT = process.env.PORT || 3000
 
 export const app = express()
 app.use(express.json())
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 const shouldConnectToDb =
   process.env.NODE_ENV !== "test" && Boolean(process.env.MONGODB_URI)
